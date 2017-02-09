99ce4bc6a5622fcc62d607cb8efdbe7dbb84d1f5
Roger-Lighthouse <roger@whiteshark.ca>
2017-02-09 17:40:07 +0000

    map-challenge ok!!

---
+++ map-challenge.js
@@ -0,0 +1,20 @@
+function p(item){
+  console.log(item);
+}
+
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+
+var result = input.map(function(item){
+  x=item['x'];
+  y=item['y'];
+  return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
